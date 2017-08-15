83d35cf48900ad08ba1b58e7c47b00c47e61407b
bopcheshailesh <bopche0702@gmail.com>
2017-08-16 00:18:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-    res.send('Arcticle one is requested and has been reponded.');
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two',function(req,res){
