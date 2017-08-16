d725ca27657ff4474984c2f4867ef2f38cdc251d
bopcheshailesh <bopche0702@gmail.com>
2017-08-16 19:06:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.get('/', function (req, res) {
 var counter = 0;
 app.get('/counter', function(req, res){
     counter = counter + 1;
-    res.send(counter.tostring());
+    res.send(counter.toString());
 });
 
 app.get('/:x',function(req,res){
