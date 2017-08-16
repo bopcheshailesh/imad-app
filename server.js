5be3ffc7577e9d04fc1909994ac4aa680a89cd8f
bopcheshailesh <bopche0702@gmail.com>
2017-08-16 16:55:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,11 +85,11 @@ app.get('/article-one',function(req,res){
 });
 
 app.get('/article-two',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+    res.send(callme(articletwo));
 });
 
 app.get('/article-three',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+    res.send(callme(articlethree));
 });
 
 app.get('/ui/style.css', function (req, res) {
