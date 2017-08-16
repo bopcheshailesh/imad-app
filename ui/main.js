16cbfa272df69097548724e0d07eb88d343013bf
bopcheshailesh <bopche0702@gmail.com>
2017-08-16 18:46:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,8 +3,15 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = 'New text';
 
-var addLeftmargin = document.getElementById('madi');
+var img = document.getElementById('madi');
 
-addLeftmargin.onclick = function(){
-    
+var marginLeft = 0;
+
+function moveright(){
+    marginLeft = marginLeft + 1 ;
+    img.style.margin-left = marginleft + 'px';
+}
+
+img.onclick = function(){
+    var interval = setInterval(moveright, 50);
 };
