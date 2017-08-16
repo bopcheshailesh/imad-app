12079629722636b142fbea0fafcd1ef119e4ac54
bopcheshailesh <bopche0702@gmail.com>
2017-08-16 18:33:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,10 @@
 console.log('Loaded!');
+
 var element = document.getElementById('main-text');
 element.innerHTML = 'New text';
+
+var addLeftmargin = document.getElementById('madi');
+
+addLeftmargin.onclick = function(){
+    
+};
