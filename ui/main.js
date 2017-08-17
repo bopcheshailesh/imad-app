4f473aa700c7509575d336721f74980d519e1c8a
bopcheshailesh <bopche0702@gmail.com>
2017-08-17 13:03:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -44,5 +44,17 @@ button.onclick = function(){
    //Make the Request
    request.open('GET', 'http://bopche070269.imad.hasura-app.io/counter', true);
    request.send(null);
-   
 }
+
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onClick = function(){
+    var names = ['name1', 'name2', 'name3', 'name4'];
+    var list = '';
+    for (var i = 0; i < names.length; i++){
+        list += '<li>' + 'names[i]' + '</li>';        
+    }
+    var ul = document.getElementById('nameList');
+    ul.innerHTML = list;
+};
